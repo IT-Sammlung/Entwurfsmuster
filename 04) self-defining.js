a80304b9c3d47887e7612eaa041a1ce8e3743dcc
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-02 16:57:35 +0200

    Update 04) self-defining.js

--- 04) self-defining.js
+++ 04) self-defining.js
@@ -1,10 +1,12 @@
-// Funktion überschreibt sich selbst
 function log1() {
   console.log("1");
   log1 = function log2() {
     console.log("2");
   }
 }
+const referenz = log1; // <-- Funktionsreferenz
 log1(); // 1
 log1(); // 2
 log1(); // 3
+referenz(); // 1  
+referenz(); // 1
