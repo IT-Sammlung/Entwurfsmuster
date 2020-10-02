08c004a56fff61f621ada3f95a183621584f58fe
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-02 16:47:32 +0200

    Update 03) Callback.js

--- 03) Callback.js
+++ 03) Callback.js
@@ -1,3 +1,4 @@
+// Einfaches Beispiel einer Callback-Funktion (Rückruffunktion)
 function fun1(callback) {
   console.log("Vor Callback");
   callback();
@@ -8,3 +9,28 @@ function fun2() {
 }
 
 fun1(fun2);
+
+
+// Asynchrone Funktion die nicht funktioniert, da sie sofort (synchron) einen Wert zurückgibt welcher noch nicht bekannt ist, also undef
+function async1() {
+  setTimeout(() => {
+    let x = 8000;
+    console.log(x);
+    return x;
+  }, 2000);
+}
+console.log(async1()); //undefined --> nach 2sekunden 8000
+
+// Asynchrone Funktion die funktioniert mit dem Callback-Entwurfsmuster
+function async2(callback) {
+  setTimeout(() => {
+    let x = 4000;
+    console.log(x);
+    callback(x);
+  }, 2000);
+}
+async2(
+  (x) => {
+    console.log(x); // 4000 nach 2 Sekunden (2x);
+  }
+);
