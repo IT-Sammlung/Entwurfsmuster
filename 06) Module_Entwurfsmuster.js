d3a6bba411e5783e65d57b4376ca8393417838d6
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-06 16:25:36 +0200

    Update 06) Module_Entwurfsmuster.js

--- 06) Module_Entwurfsmuster.js
+++ 06) Module_Entwurfsmuster.js
@@ -1,18 +1,40 @@
-      // Kapselung von Daten über IIFE und Closure
-      // Module Entwurfsmuster
-      const Modul = (() => {
-        let _zahl = 12;
-        return {
-          log: function() {
-            console.log(_zahl);
-          },
-          insert: function(x) {
-            _zahl = x;
-          }
-        }
-      })();
-      Modul.log();
-      Modul._zahl = 15;
-      Modul.log();
-      Modul.insert(5);
-      Modul.log();
+// Kapselung von Daten über IIFE und Closure
+// Module Entwurfsmuster
+const Modul = (() => {
+  let _zahl = 12;
+  return {
+    log: function() {
+      console.log(_zahl);
+    },
+    insert: function(x) {
+      _zahl = x;
+    }
+  }
+})();
+Modul.log();
+Modul._zahl = 15;
+Modul.log();
+Modul.insert(5);
+Modul.log();
+
+
+
+
+
+// Module Entwurfsmuster
+const Modul = (() => {
+  let _zahl = 12;
+  return {
+    log: function() {
+      console.log(_zahl);
+    },
+    insert: function(x) {
+      _zahl = x;
+    },
+    double: function(x) {
+      Modul.insert(x*2); // <-- Aufruf anderer Funktion
+    }
+  }
+})();
+Modul.double(5);
+Modul.log();
